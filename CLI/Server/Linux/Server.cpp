5815f1343a16831cd1573fc666e0c8f7b7ad8aa2
Ivan Kirsanov <imora@student.42.fr>
2025-08-28 03:49:00 +0200

    Added signal handlers for CLI

--- CLI/Server/Linux/Server.cpp
+++ CLI/Server/Linux/Server.cpp
@@ -3,11 +3,23 @@
 #include <vector>
 #include <chrono>
 #include <thread>
+#include <csignal>
+
+bool working = true;
+
+void OnExit(int)
+{
+    Stop();
+    working = false;
+}
 
 int main(int argc, char** argv)
 {
     Start(argv, argc - 1);
-    while (true)
+    std::signal(SIGINT,  OnExit);
+    std::signal(SIGTERM, OnExit);
+
+    while (working)
     {
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
